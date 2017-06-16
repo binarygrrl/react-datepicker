cd2698bcfd06774f4898d74d1decb869bd92a7d7
Amber <binarygrrl@gmail.com>
2017-06-16 14:39:38 -0700

    Make updates to example app

--- examples/hello-world/src/App.js
+++ examples/hello-world/src/App.js
@@ -5,27 +5,78 @@ import moment from 'moment';
 import 'react-datepicker/dist/react-datepicker.css';
 
 class App extends Component {
-  constructor() {
+/*  constructor() {
     super();
     this.state = {
       startDate: moment()
     };
+  }*/
+
+   constructor (props) {
+    super(props)
+    this.state = {
+      startDate: moment('2017-06-08'),
+      endDate: moment('2017-06-10')
+    }
   }
 
-  render() {
-    return (
-      <DatePicker
-        selected={this.state.startDate}
-        onChange={this.handleChange.bind(this)}
-      />
-    );
+  handleChange = ({ startDate, endDate }) => {
+    startDate = startDate || this.state.startDate
+    endDate = endDate || this.state.endDate
+
+    if (startDate.isAfter(endDate)) {
+      var temp = startDate
+      startDate = endDate
+      endDate = temp
+    }
+
+    this.setState({ startDate, endDate })
   }
 
-  handleChange(date) {
-    this.setState({
-      startDate: date
-    });
+  handleChangeStart = (startDate) => this.handleChange({ startDate })
+
+  handleChangeEnd = (endDate) => this.handleChange({ endDate })
+
+  render () {
+    return <div className="row">
+        <div className="column">
+            <DatePicker
+                selected={this.state.startDate}
+                selectsStart
+                startDate={this.state.startDate}
+                endDate={this.state.endDate}
+                onChange={this.handleChangeStart} 
+                dateFormat="MMM, d YYYY"
+                className=""
+                calendarClassName=""                
+                placeholderText="Select Start Date"
+            />
+            <DatePicker
+                selected={this.state.endDate}
+                selectsEnd
+                startDate={this.state.startDate}
+                endDate={this.state.endDate}
+                onChange={this.handleChangeEnd}
+                dateFormat="MMM, d YYYY"
+                className=""
+                calendarClassName=""                
+                placeholderText="Select End Date"
+            />
+        </div>
+        <div className="column">
+            <h3>Custom Date Picker</h3>
+            <DatePicker
+                selected={this.state.startDate}
+                onChange={this.handleChange}
+                dateFormat="MMM, d YYYY"
+                className=""
+                calendarClassName=""                
+                placeholderText="Select Date"                
+                monthsShown={2}/>
+        </div>
+    </div>
   }
+
 }
 
 export default App;
