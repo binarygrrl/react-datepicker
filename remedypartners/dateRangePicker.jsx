6c35c40ee0857632e0d9ec2d7918080494402262
Amber <binarygrrl@gmail.com>
2017-06-16 15:14:06 -0400

    create new components

--- remedypartners/dateRangePicker.jsx
+++ remedypartners/dateRangePicker.jsx
@@ -11,7 +11,66 @@ Custom Input
 Multiple Months
 /
 
-
 /*  Examples to Include: Default  |  Custom Date Format  |  Custom CLass Name  |  Custom Calendar Classname  |  Placeholder Text  |  Date Range  |  Disable  |   Fixed Height  |  Custom Input  |  Multiple Months  */
 
+import React from 'react'
+import DatePicker from 'react-datepicker'
+import moment from 'moment'
 
+export default class DateRangePicker extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      startDate: moment('2017-06-08'),
+      endDate: moment('2017-06-10')
+    }
+  }
+
+  handleChange = ({ startDate, endDate }) => {
+    startDate = startDate || this.state.startDate
+    endDate = endDate || this.state.endDate
+
+    if (startDate.isAfter(endDate)) {
+      var temp = startDate
+      startDate = endDate
+      endDate = temp
+    }
+
+    this.setState({ startDate, endDate })
+  }
+
+  handleChangeStart = (startDate) => this.handleChange({ startDate })
+
+  handleChangeEnd = (endDate) => this.handleChange({ endDate })
+
+  render () {
+    return <div className="row">
+        <div className="column">
+            <DatePicker
+                selected={this.state.startDate}
+                selectsStart
+                startDate={this.state.startDate}
+                endDate={this.state.endDate}
+                onChange={this.handleChangeStart} />
+            <DatePicker
+                selected={this.state.endDate}
+                selectsEnd
+                startDate={this.state.startDate}
+                endDate={this.state.endDate}
+                onChange={this.handleChangeEnd}/>
+            // Custom date range picker selection
+            <DatePicker
+                // customInput={<ExampleCustomInput />}
+                dateFormat="MMM, d YYYY"
+                selected={this.state.startDate}
+                onChange={this.handleChange}
+                className="red-border"
+                calendarClassName="rasta-stripes"
+                disabled
+                placeholderText="Select Start Date"
+                fixedHeight
+                monthsShown={2}/>
+        </div>
+    </div>
+  }
+}
--- remedypartners/singleDatePicker.jsx
+++ remedypartners/singleDatePicker.jsx
@@ -1,3 +1,55 @@
 /*   Examples to Include: Default  |  Custom Date Format  |  Custom CLass Name  |  Custom Calendar Classname  |  Placeholder Text  |  Disable  |   Fixed Height  |  Custom Input  */
+import React from 'react'
+import PropTypes from 'prop-types'
+import DatePicker from 'react-datepicker'
+import moment from 'moment'
 
+/* eslint-disable react/no-multi-comp */
+class ExampleCustomInput extends React.Component {
+  static propTypes = {
+    onClick: PropTypes.func,
+    value: PropTypes.string
+  }
 
+  render () {
+    return (
+      <button
+          className="example-custom-input"
+          onClick={this.props.onClick}>
+          {this.props.value}
+      </button>
+    )
+  }
+}
+
+export default class SingleDatePicker extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      startDate: moment()
+    }
+  }
+
+  handleChange = (date) => {
+    this.setState({
+      startDate: date
+    })
+  }
+
+  render () {
+    return <div className="row">
+        <div className="column">
+            <DatePicker
+                customInput={<ExampleCustomInput />}
+                dateFormat="MMM, d YYYY"
+                selected={this.state.startDate}
+                onChange={this.handleChange}
+                className="red-border"
+                calendarClassName="rasta-stripes"
+                disabled
+                placeholderText="Select Date"
+                fixedHeight />
+      </div>
+    </div>
+  }
+}
