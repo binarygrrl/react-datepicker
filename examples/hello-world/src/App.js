e595feeb6916878cd4edaffad92a83c3268f5529
binarygrrl <binarygrrl@gmail.com>
2017-06-19 10:50:01 -0700

    update break down of date pickers

--- examples/hello-world/src/App.js
+++ examples/hello-world/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
+import SingleDatePicker from './SingleDatePicker'
+import DateRangePicker from './DateRangePicker'
 
 import 'react-datepicker/dist/react-datepicker.css';
 import './remedyStyles.css';
@@ -41,45 +43,63 @@ class App extends Component {
   handleChangeEnd = (endDate) => this.handleChange({ endDate })
 
   render () {
-    return <div className="row">
-        <div className="column">
-            <DatePicker
-                selected={this.state.startDate}
-                selectsStart
-                startDate={this.state.startDate}
-                endDate={this.state.endDate}
-                onChange={this.handleChangeStart}
-                dateFormat="MMM, D YYYY"
-                className=""
-                calendarClassName=""
-                placeholderText="Select Start Date"
-            />
-            <DatePicker
-                placeholderText="Select End Date"
-                selected={this.state.endDate}
-                selectsEnd
-                startDate={this.state.startDate}
-                endDate={this.state.endDate}
-                onChange={this.handleChangeEnd}
-                dateFormat="MMM, D YYYY"
-                className=""
-                calendarClassName=""
-            />
+    return <div class="datePickWrap">
+        <div className="row">
+          <div className="column">
+
+            <h3>App Range Picker</h3>
+              <DatePicker
+                  selected={this.state.startDate}
+                  selectsStart
+                  startDate={this.state.startDate}
+                  endDate={this.state.endDate}
+                  onChange={this.handleChangeStart}
+                  dateFormat="MMM, D YYYY"
+                  className=""
+                  calendarClassName=""
+                  placeholderText="Select Start Date"
+              />
+              <DatePicker
+                  placeholderText="Select End Date"
+                  selected={this.state.endDate}
+                  selectsEnd
+                  startDate={this.state.startDate}
+                  endDate={this.state.endDate}
+                  onChange={this.handleChangeEnd}
+                  dateFormat="MMM, D YYYY"
+                  className=""
+                  calendarClassName=""
+              />
+          </div>
         </div>
-        <div className="column">
-            <h3>Multi Date View</h3>
-            <DatePicker
-                placeholderText="Select Date"
-                selected={this.state.startDate}
-                onSelect={this.state.startDate}
-                onChange={this.handleChange}
-                startDate={this.state.startDate}
-                endDate={this.state.endDate}
-                dateFormat="MMM, D YYYY"
-                className=""
-                calendarClassName=""                
-                //monthsShown={2}
-                />
+        <div className="row">
+          <div className="column">
+              <h3>Multi Date View</h3>
+              <DatePicker
+                  placeholderText="Select Date"
+                  selected={this.state.startDate}
+                  onSelect={this.state.startDate}
+                  onChange={this.handleChange}
+                  startDate={this.state.startDate}
+                  endDate={this.state.endDate}
+                  dateFormat="MMM, D YYYY"
+                  className=""
+                  calendarClassName=""                
+                  monthsShown={2}
+                  />
+          </div>
+        </div>
+        <div className="row">
+          <div className="column">
+            <h3>Single Date Picker</h3>
+            <SingleDatePicker></SingleDatePicker>
+          </div>
+        </div>
+        <div className="row">
+          <div className="column">
+            <h3>Date Range Picker</h3>
+            <DateRangePicker></DateRangePicker>
+          </div>
         </div>
     </div>
   }
--- examples/hello-world/src/index.js
+++ examples/hello-world/src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import SingleDatePicker from './SingleDatePicker'
-import DateRangePicker from './DateRangePicker'
 
 ReactDOM.render(
   <App />,
-  //<SingleDatePicker />,
-  //<DateRangePicker />,
   document.getElementById('root')
 );
